a8ef92e8fda5d8dc9b924b6188d733fe147ef0b6
yestool <yestool@gmail.com>
2024-05-16 11:47:41 +0800

    add env

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger';
 import { serveStatic } from '@hono/node-server/serve-static'
 import index from './pages';
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
@@ -11,6 +12,14 @@ app.use(logger())
 app.use('/static/*', serveStatic({ root: './' }))
 
 app.route('/', index)
+
+
+app.get('/env', (c) => {
+  // NAME is process.env.NAME on Node.js or Bun
+  // NAME is the value written in `wrangler.toml` on Cloudflare
+  const { NAME } = env<{ NAME: string }>(c)
+  return c.text(NAME)
+})
 
 const port = 3000;
 
